cf8c490bfa72d46ccdf3481c0baea37b71096276
AnjaSamsom <anjavt14@gmail.com>
2022-03-27 22:35:25 -0400

    changed title

--- top.php
+++ top.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="utf-8" />
     <meta name="author" content="Anja Samsom" />
-    <title>Lyric Comparison</title>
+    <title>Anja Samsom</title>
     <meta name="description" content="" />
     <link
       href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
