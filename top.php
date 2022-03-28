c238221354ccefb13fd3fe3f604e9e2fca22f438
AnjaSamsom <anjavt14@gmail.com>
2022-03-27 22:33:55 -0400

    updates php

--- top.php
+++ top.php
@@ -39,30 +39,3 @@
           </div>
         </div>
       </nav>
-
-    <div>
-      <h1 class="text-center" style = "color: #0B3923;">Anja Samsom</h1>
-    </div>
-    
-  </body>
-
-
-<!-- bootstrap -->
-<div class="container">
-    <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
-      <div class="col-md-4 d-flex align-items-center">
-        <a href="/" class="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1">
-          <svg class="bi" width="30" height="24"><use xlink:href="#bootstrap"/></svg>
-        </a>
-        <span class="text-muted">&copy; 2022 Anja Samsom</span>
-      </div>
-  
-      <ul class="nav col-md-4 justify-content-end list-unstyled d-flex">
-        <li class="ms-3"><a class="text-muted" href="https://github.com/AnjaSamsom"><svg class="bi" width="24" height="24"><use xlink:/></svg>github</a></li>
-        <li class="ms-3"><a class="text-muted" href="https://www.instagram.com/superanja14/"><svg class="bi" width="24" height="24"><use xlink:/></svg>instagram</a></li>
-        <li class="ms-3"><a class="text-muted" href="https://heyanja.tumblr.com/"><svg class="bi" width="24" height="24"><use xlink:/></svg>tumblr</a></li>
-      </ul>
-    </footer>
-  </div>
-
-</html>
