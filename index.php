7f1470cdf3af866028ce3f64bb3f9cddc2ab663b
AnjaSamsom <anjavt14@gmail.com>
2022-03-27 22:37:57 -0400

    added photo

--- index.php
+++ index.php
@@ -2,7 +2,8 @@
     <div>
       <h1 class="text-center" style = "color: #0B3923;">Anja Samsom</h1>
     </div>
-    
+    <img src="anja_photo.jpg" alt="Anja's photo" width="500" height="600">
+
   </body>
 <?php include "footer.php";?>
 
